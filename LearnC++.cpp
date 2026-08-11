bb0d0801ada5cc35c68d5e1abc95416a14c2afd0
Bharat <bandanupretibisht@gmail.com>
2026-08-11 15:28:28 +0530

    Day-1 Pattern3

--- LearnC++.cpp
+++ LearnC++.cpp
@@ -4,7 +4,7 @@ void print1(int n)
 {
     for (int a = 0; a < n; a++)
     {
-        for (int b = 0; b <= a; b++)
+        for (int b = 0; b < n - a; b++)
         {
             cout << "*";
         }
